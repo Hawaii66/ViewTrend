1dc7bbd8cbade16a992b8eb0213e1b53c43d0a78
Sebastian <hawaiilive@outlook.com>
2024-02-25 21:41:39 +0100

    links

--- viewtrend/src/app/page.tsx
+++ viewtrend/src/app/page.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function Home() {
-  return <></>;
+  return (
+    <div>
+      <div>
+        <h1>ViewTrend</h1>
+        <nav>
+          <li>
+            <Link href={"/predict"}>
+              <Button>Predict</Button>
+            </Link>
+            <Link href={"/howto"}>
+              <Button>How to</Button>
+            </Link>
+            <Link href={"/me"}>
+              <Button>Me</Button>
+            </Link>
+          </li>
+        </nav>
+      </div>
+    </div>
+  );
 }
